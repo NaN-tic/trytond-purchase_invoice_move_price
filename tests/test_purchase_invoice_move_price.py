0ed1bfca4f70b8a4d58a376976bdd3badc23ed89
Jes?s Mart?n Jim?nez <jesus@nan-tic.com>
2016-08-22 10:50:05 +0200

    Upgrade test to version 4.0

--- tests/test_purchase_invoice_move_price.py
+++ tests/test_purchase_invoice_move_price.py
@@ -3,19 +3,14 @@
 import unittest
 import doctest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 from trytond.tests.test_tryton import doctest_setup, doctest_teardown
+from trytond.tests.test_tryton import doctest_checker
 
 
-class TestCase(unittest.TestCase):
+class TestCase(ModuleTestCase):
     'Test module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('purchase_invoice_move_price')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'purchase_invoice_move_price'
 
 
 def suite():
@@ -23,5 +18,6 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestCase))
     suite.addTests(doctest.DocFileSuite('scenario_purchase.rst',
             setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
+            checker=doctest_checker,
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
