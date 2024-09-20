abb4ba684f56497e5887613da018faa0a5747c44
nan-tic-dev <79672209+nan-tic-dev@users.noreply.github.com>
2024-09-20 14:03:40 +0200

    #157531 (#1)
    * Adapt module to purchase_discount | #157531
    
    * Add test scenario
    
    ---------
    
    Co-authored-by: Jared Esparza <jared.esparza@nan-tic.com>

--- purchase.py
+++ purchase.py
@@ -9,9 +9,13 @@ class PurchaseLine(metaclass=PoolMeta):
     __name__ = 'purchase.line'
 
     def get_invoice_line(self):
-        lines = super(PurchaseLine, self).get_invoice_line()
+        lines = super().get_invoice_line()
         if self.purchase.invoice_method == 'shipment':
             for line in lines:
                 if line.stock_moves:
                     line.unit_price = line.stock_moves[0].unit_price
+                    if hasattr(line.__class__, 'base_price'):
+                        if line.stock_moves[0].unit_price != line.stock_moves[0].origin.unit_price:
+                            line.base_price = None
+                            line.discount = None
         return lines
